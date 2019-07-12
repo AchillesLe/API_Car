a7952c28513e5200d53766601d840c665193a8c9
AchillesLe <lvbao.cntt.sgu@gmail.com>
2019-07-12 13:08:08 +0700

    add nodemon and mongoose

--- app.js
+++ app.js
@@ -2,22 +2,18 @@ const express = require('express');
 
 const logger = require('morgan');
 const app = express();
+const users_url = require('./routes/users');
 
 //Midddewares
+app.use(logger('dev'))
+app.use('/users',users_url);
 
-//Routes
-app.get('/',(req,res,next)=>{
-    res.status(200).json({
-        message  : 'you requested index page'
-    })
-});
-// Catch 404 Errors and forward them to error handler
 app.use( (req,res,next)=>{
     const err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
-// Error handler function
+
 app.use( (err,req,res,next)=>{
     const error = app.get('env') === 'development' ? err : {}
     const status = err.status || 500;
@@ -29,7 +25,6 @@ app.use( (err,req,res,next)=>{
     })
 });
 
-// Start the server
 const port = app.get('port') || 3000;
 app.listen(port,()=>{
     console.log(`Server is listening on port on ${port}`);
